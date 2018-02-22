83a07d24b66fe77468e16d0c29eeaa964a041020
likai <likai@dituwuyou.com>
2018-02-22 11:50:57 +0800

    lover页检测密码优化

--- I_LOVE_U/jscss/check.js
+++ I_LOVE_U/jscss/check.js
@@ -1,6 +1,4 @@
-$(function(){
-    var checkStatus = window.localStorage.getItem('love_day') === '20180124true';
-    if(!checkStatus){
-        window.location.href = 'http://www.91lik.win/';
-    }
-});
+var checkStatus = window.localStorage.getItem('love_day') === '20180124true';
+if(!checkStatus){
+    window.location.href = 'http://www.91lik.win/';
+}
